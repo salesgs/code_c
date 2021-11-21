887f34c07ef465f6a4d625b2db79c5bec4c32f1e
Gabriel Sales <93557539+salesgs@users.noreply.github.com>
2021-11-20 22:28:33 -0300

    Add files via upload

---
+++ Tabuada com for.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int n,c;
+
+printf("Tabuada  \n");
+printf("digite o numero \n");
+scanf("%i",&n);
+for(c=1;c<=10;c++){
+printf("\n %i X %i= %i \n ",n,c,n*c);	
+	
+}
+
+
+return 0;
+}
