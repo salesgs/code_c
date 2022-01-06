b82d45fddfa8f725c83b284861b1f4a219687123
Gabriel Sales <93557539+salesgs@users.noreply.github.com>
2022-01-06 20:40:45 -0300

    Add files via upload

---
+++ Perguntas Matematicas.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+int main(){
+   	int   a=4,b=6,c=5,d=10,e=21;        //   respostas
+     int  aa,bb,cc,dd,ee;            //var das repostas que vão ser comparadas           
+
+printf("Qaul resultado da soma 2+2? digite sua resposta  \n");
+     scanf("%i",&aa);
+
+      if(aa!=a){
+         printf("Voce errou :( \n\n");
+		 }
+          else{
+	       printf("Voce acertou \n\n");
+	       }            
+
+printf("Qaul resultado da soma 5+1? digite sua resposta  \n");
+   scanf("%i",&bb);
+
+        if(bb!=b){
+		 printf("Voce errou :( \n\n");
+		 } 
+            else{
+	        printf("Voce acertou \n\n");
+	        }
+
+printf("Qaul resultado da soma 3+2? digite sua resposta  \n");
+   scanf("%i",&cc); 
+ 
+           if(cc!=c){
+		 printf("Voce errou :( \n\n");
+		 } 
+              else{
+	        printf("Voce acertou");
+	        }
+
+printf("Qaul resultado da soma 8+2? digite sua resposta  \n"); 
+     scanf("%i",&dd); 
+ 
+  if(dd!=d){
+		 printf("Voce errou :( \n\n");
+		 } 
+            else{
+	        printf("Voce acertou \n\n");
+	        }
+printf("Qaul resultado da soma 20+1? digite sua resposta  \n"); 
+     scanf("%i",&ee); 
+
+       
+         if(ee!=e){
+		  printf("Voce errou :( \n\n");
+		 } 
+            else{
+	        printf("Voce acertou \n\n");
+	        }
+
+
+ 
+
+return 0;	
+}
+
+
+//progama para crianças que estão aprendendo a somar
